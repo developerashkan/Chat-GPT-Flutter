cf4d57c8cdd4e6e98c3688480f42f3446b5e36fb
Ashkan Bahmani <74037732+developerashkan@users.noreply.github.com>
2026-07-05 13:13:49 +0400

    Update splash_screen.dart

--- lib/splash_screen.dart
+++ lib/splash_screen.dart
@@ -3,28 +3,38 @@ import 'package:chatgpt/chat_screen.dart';
 import 'package:flutter/material.dart';
 
 class SplashScreen extends StatefulWidget {
-  const SplashScreen({Key? key}) : super(key: key);
+  const SplashScreen({super.key});
 
   @override
   State<SplashScreen> createState() => _SplashScreenState();
 }
 
 class _SplashScreenState extends State<SplashScreen> {
-
   @override
   void initState() {
-    Timer(
-        const Duration(seconds: 2),
-            () => Navigator.of(context).pushReplacement(MaterialPageRoute(
-            builder: (BuildContext context) => const ChatScreen())));
-
     super.initState();
+    _navigateToHome();
+  }
+
+  Future<void> _navigateToHome() async {
+    await Future.delayed(const Duration(seconds: 2));
+        if (!mounted) return; 
+
+    Navigator.of(context).pushReplacement(
+      MaterialPageRoute(builder: (_) => const ChatScreen()),
+    );
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(child: Image.asset("assets/logo-no-background.png",width: 250,height: 250,),),
+      body: Center(
+        child: Image.asset(
+          "assets/logo-no-background.png",
+          width: 250,
+          height: 250,
+        ),
+      ),
     );
   }
 }
